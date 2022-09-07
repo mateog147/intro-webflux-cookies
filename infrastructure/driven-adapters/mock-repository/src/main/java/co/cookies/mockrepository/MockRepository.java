7c85475a975b80223cdade1e69ba69ea3f849fdc
mateosofka <mateo.gutierrez@sofka.com.co>
2022-09-07 16:35:48 -0500

    added 10 elements to mock repository

--- infrastructure/driven-adapters/mock-repository/src/main/java/co/cookies/mockrepository/MockRepository.java
+++ infrastructure/driven-adapters/mock-repository/src/main/java/co/cookies/mockrepository/MockRepository.java
@@ -11,9 +11,17 @@ import java.util.List;
 @Repository
 public class MockRepository implements CookieTypeRepository {
 
-    private List<CookieType> list = Arrays.asList(
+    private final List<CookieType> list = Arrays.asList(
                     CookieType.builder().code(1).type("Stroopwafel").build(),
-                    CookieType.builder().code(2).type("Chocolate Chip").build()
+                    CookieType.builder().code(2).type("Chocolate Chip").build(),
+                    CookieType.builder().code(3).type("Macaroons").build(),
+                    CookieType.builder().code(4).type("Oatmeal").build(),
+                    CookieType.builder().code(5).type("Fortune cookie").build(),
+                    CookieType.builder().code(6).type("Sugar").build(),
+                    CookieType.builder().code(7).type("Fried cookie").build(),
+                    CookieType.builder().code(8).type("Benne Wafer").build(),
+                    CookieType.builder().code(9).type("Oreo").build(),
+                    CookieType.builder().code(10).type("Peanut butter").build()
             );
 
     @Override
